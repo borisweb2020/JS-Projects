7dea90b9104b3884103ef00c2bcdded9570ee499
borisweb2020 <49887284+borisweb2020@users.noreply.github.com>
2023-05-05 19:10:35 +0700

    Complete time_differences.js

--- 3_Date_Objects/time_difference.js
+++ 3_Date_Objects/time_difference.js
@@ -1,12 +1,56 @@
 function getResult(distance, speed, step1, step2) {
-		let totalTime, speedDinosaur, speedPerfectDinosaur, timeDisplay;
+		let speedDinosaur, speedPerfectDinosaur, timeDisplay;
 		let timeDifference = 0;
-		totalTime = distance / speed;
-		speedPerfectDinosaur = step1 / totalTime;
-		speedDinosaur = step2 / totalTime;
+		let hours, minutes, seconds;
+		let isNegative = false;
+		//*** Calculate time difference in seconds:
+		speedPerfectDinosaur = distance / (step1 + speed);
+		speedDinosaur = distance / (step2 + speed);
+		timeDifference = Math.trunc(speedPerfectDinosaur - speedDinosaur);
+		if (timeDifference < 0) {
+				timeDifference = Math.abs(timeDifference);
+				isNegative = true;
+		}
+		//*** Calculate Hours, Minutes, Seconds:
+		if (timeDifference >= 3600) {
+			hours = Math.trunc(timeDifference / 3600);
+			if (hours < 10) {
+					hours = `0${hours}`;
+			}
+			minutes = Math.trunc((timeDifference % 3600) / 60);
+			if (minutes < 10) {
+					minutes = `0${minutes}`;
+			}
+			seconds = (timeDifference % 3600) % 60;
+			if (seconds < 10) {
+					seconds = `0${seconds}`;
+			}
+		} else if (timeDifference >= 60) {
+			hours = '00';
+			minutes = Math.trunc(timeDifference / 60);
+			if (minutes < 10) {
+					minutes = `0${minutes}`;
+			}
+			seconds = timeDifference % 60;
+			if (seconds < 10) {
+					seconds = `0${seconds}`;
+			}
+		} else {
+			hours = '00';
+			minutes = '00';
+			seconds = timeDifference;
+			if (seconds < 10) {
+					seconds = `0${seconds}`;
+			}
+		}
 
-		timeDifference = (speedDinosaur - speedPerfectDinosaur) * 10000;
-		timeDisplay = `00:00:${timeDifference}`;
+		timeDisplay = `+${hours}:${minutes}:${seconds}`;
+
+		if (isNegative) {
+				seconds = Math.abs(seconds);
+				timeDisplay = `-${hours}:${minutes}:${seconds}`;
+		}
+
 		return timeDisplay;
 }
 
